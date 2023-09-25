a1e0f1e6a0a5c2eef6f156635e98b902a5768533
dunitrashuk <mamaligadumitras@gmail.com>
2023-09-25 11:29:00 +0300

    router initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import ProductDetails from "./pages/ProductDetails";
+
+import Header from "./components/layout/header/Header";
+import Navbar from "./components/layout/nav/Navbar";
+
 function App() {
   return (
-    <>
-      <h1 className="heading-1">Hekto</h1>
-    </>
+    <Router>
+      <Header />
+      <Navbar />
+      
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/shop" element={<Shop />}></Route>
+        <Route exact path="/product" element={<ProductDetails />}></Route>
+        <Route exact path="/cart" element={<Cart />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
