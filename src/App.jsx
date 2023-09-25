310ca6cfb6263d2370f09a8e62d1b7ba3519d7a0
dunitrashuk <mamaligadumitras@gmail.com>
2023-09-25 16:02:02 +0300

    footer section

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ import ProductDetails from "./pages/ProductDetails";
 
 import Header from "./components/layout/header/Header";
 import Navbar from "./components/layout/nav/Navbar";
+import Footer from "./components/layout/footer/Footer";
 
 function App() {
   return (
     <Router>
       <Header />
       <Navbar />
-      
+
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/shop" element={<Shop />}></Route>
         <Route exact path="/product" element={<ProductDetails />}></Route>
         <Route exact path="/cart" element={<Cart />}></Route>
       </Routes>
+
+      <Footer />
     </Router>
   );
 }
